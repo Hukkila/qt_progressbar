3fb11ef60f104b87f42f3e89650193f01c1eeae1
Filip Hukkila <90825463+Hukkila@users.noreply.github.com>
2023-09-19 09:08:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from qt_file import main
+from sub import MainClass
 
+def main():
+    print('Running, typ y')
+    user_input = input()
+    if user_input == 'y':
+        class1 = MainClass
+        class1.sub_main(self=MainClass)
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
-

d9db929c0a79130718edbeef4d56078583aa6dc9
Filip Hukkila <90825463+Hukkila@users.noreply.github.com>
2023-09-19 09:07:45 +0200

    Update sub.py

--- sub.py
+++ sub.py
@@ -1,12 +1,13 @@
 
+# Import the necessary modules from gui_file.py
+
+from qt_file import MyDialog
 
 
 
-class DataMaker:
-    def data_sender(self):
-        data = [['first', 10], ['second', 20, 2000],['third', 10],
-                ['fourth', 30, 30000], ['fifth', 20], ['sixth', 10]]  # Third value represents time in milliseconds
-        return data
-    
 
+class MainClass:
 
+    def sub_main(self):     
+        class1= MyDialog
+        class1.main_gui()

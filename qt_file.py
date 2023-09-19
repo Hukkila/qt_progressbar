8a2c1f9f3f0850589bb522980db30cd9bca9db65
Filip Hukkila <90825463+Hukkila@users.noreply.github.com>
2023-09-19 09:07:22 +0200

    Update qt_file.py

--- qt_file.py
+++ qt_file.py
@@ -1,63 +1,58 @@
 import sys
-import time
 from PyQt5.QtWidgets import QApplication, QDialog, QListView, QProgressBar
 from PyQt5.QtCore import QTimer
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
-from sub import DataMaker
+
+
+class DataMaker:
+    def data_sender(self):
+        data = [['first', 10], ['second', 20, 2000],['third', 10],
+                ['fourth', 30, 10000], ['fifth', 20], ['sixth', 10]]
+        return data
+
 
 class MyDialog(QDialog):
-    total_percent = 0  # Class attribute to accumulate total percentage
+    total_percent = 0  
 
     def __init__(self):
         super().__init__()
-
-        # Load the .ui file dynamically
         loadUi("Dialog.ui", self)
 
-        # Access UI elements
-        self.buttonBox.accepted.connect(self.on_accepted)  # Connect accepted signal
-        self.buttonBox.rejected.connect(self.on_rejected)  # Connect rejected signal
+        self.buttonBox.accepted.connect(self.on_accepted)  
+        self.buttonBox.rejected.connect(self.on_rejected)  
         self.listView = self.findChild(QListView, 'listView')
         self.progressBar = self.findChild(QProgressBar, 'progressBar')
-
-        # Create an instance of DataMaker
-        self.data_maker_instance = DataMaker()
-
-        # Initialize a timer for displaying items one at a time
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_percent)
 
+        # Import from DataMaker class
+        self.data_maker_instance = DataMaker()
+
     def on_accepted(self):
-        # Define the action to perform when the "Ok" button is clicked
         print("Accepted")
 
     def on_rejected(self):
-        # Define the action to perform when the "Cancel" button is clicked
         print("Rejected")
 
     def data_importer(self):
         self.data = self.data_maker_instance.data_sender()
         self.current_index = 0
-        self.total_percent = 0  # Reset the total percent to 0 for each run
-        self.target_percent = 0  # Reset the target percent to 0 for each run
-
-        # Create a QStandardItemModel to hold the data
+        self.total_percent = 0  
+        self.target_percent = 0 
         self.model = QStandardItemModel()
-
-        # Start the timer with the interval from the third value in the data
         self.set_timer_interval()
 
     def set_timer_interval(self):
         if self.current_index < len(self.data):
             item_data = self.data[self.current_index]
             if len(item_data) >= 2:
-                self.target_percent += item_data[1]  # Accumulate target percent across items
+                self.target_percent += item_data[1]  
                 if len(item_data) >= 3:
-                    time_value = item_data[2]  # Third value in the list (time in ms)
-                    self.timer_interval = int(time_value / self.target_percent)  # Convert to integer
+                    time_value = item_data[2] 
+                    self.timer_interval = int(time_value / self.target_percent) 
                 else:
-                    self.timer_interval = 1000  # Default to 1 second if no time is specified
+                    self.timer_interval = 100 
                 self.timer.start(self.timer_interval)
             else:
                 self.timer.stop()
@@ -74,21 +69,20 @@ class MyDialog(QDialog):
         if self.current_index < len(self.data):
             item_data = self.data[self.current_index]
 
-            item = QStandardItem(str(item_data))
+            item = QStandardItem(str(item_data[0]))
             self.model.appendRow(item)
             self.listView.setModel(self.model)
             self.current_index += 1
-            self.set_timer_interval()  # Set timer interval for the next item
+            self.set_timer_interval()  
         else:
-            # Stop the timer when all items have been displayed
             self.timer.stop()
 
-def main():
-    app = QApplication(sys.argv)
-    dialog = MyDialog()
-    dialog.show()
-    dialog.data_importer()  # Start displaying items
-    sys.exit(app.exec_())
+    def main_gui():
+        app = QApplication(sys.argv)
+        dialog = MyDialog()
+        dialog.show()
+        dialog.data_importer()  # Start displaying items
+        sys.exit(app.exec_())
 
 
 
